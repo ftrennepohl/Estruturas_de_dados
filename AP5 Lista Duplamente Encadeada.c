e82237e6f0a7eadf9afdd22b0659c1dfa0ce4720
ftrennepohl <ftrennepohl@gmail.com>
2022-03-02 19:58:47 -0300

    Fix free

--- AP5 Lista Duplamente Encadeada.c
+++ AP5 Lista Duplamente Encadeada.c
@@ -106,12 +106,16 @@ void lenList(Aluno *f){
 void freeMem(Aluno **f){
     Aluno *aux, *prev;
     for (aux = *f; aux != NULL; aux = aux->next){
-        if (aux->next == NULL) free(aux);
-        else free(prev);
-        printf("*");
+		if (aux->next != NULL){
+			free(prev);
+			printf("*");
+		} else{
+			free(aux);
+			printf("*");
+			return;
+		}
         prev = aux;
     }
-    return;
 }
 
 int main(){
