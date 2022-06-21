fb93d444ef901446aa72cc04c13eaccd8c5463bc
Fabricio <ftrennephl@gmail.com>
2022-06-21 14:59:28 -0300

    .

--- POD_Trabalho1.c
+++ POD_Trabalho1.c
@@ -4,24 +4,24 @@
 
 typedef struct data
 {
-	int comps, swaps;
+	long int comps, swaps;
 } Data;
 
-Data bubbleSort(int *arr, int n)
+void initData(Data **res){
+	for (int i=0; i<3; i++){
+		res[i]->comps = 0;
+		res[i]->swaps = 0;
+	}
+}
+
+Data bubbleSort(int *vet, int n, Data res)
 {
-	int *vet = arr;
 	int flag;
-	Data res;
-	res.comps = 0;
-	res.swaps = 0;
-	for (int i = n - 1; i > 0; i--)
-	{
+	for (int i = n - 1; i > 0; i--){
 		flag = 0;
-		for (int j = 0; j < i; j++)
-		{
+		for (int j = 0; j < i; j++){
 			res.comps++;
-			if (vet[j] > vet[j + 1])
-			{
+			if (vet[j] > vet[j + 1]){
 				int aux = vet[j + 1];
 				vet[j + 1] = vet[j];
 				vet[j] = aux;
@@ -29,21 +29,15 @@ Data bubbleSort(int *arr, int n)
 				flag = 1;
 			}
 		}
-		if (!flag)
-		{
+		if (!flag){
 			return res;
 		}
 	}
 	return res;
 }
 
-Data selectionSort(int *arr, int n)
+Data selectionSort(int *vet, int n, Data res)
 {
-	int *vet = malloc(n * sizeof(int));
-	vet = arr;
-	Data res;
-	res.comps = 0;
-	res.swaps = 0;
 	for (int i = 0; i < n - 1; i++)
 		for (int j = i; j < n; j++)
 		{
@@ -59,12 +53,8 @@ Data selectionSort(int *arr, int n)
 	return res;
 }
 
-Data insertionSort(int *arr, int n)
+Data insertionSort(int *vet, int n, Data res)
 {
-	int *vet = arr;
-	Data res;
-	res.comps = 0;
-	res.swaps = 0;
 	for (int i = 1; i < n; i++){
 		int key = vet[i];
 		int j = i - 1;
@@ -80,23 +70,55 @@ Data insertionSort(int *arr, int n)
 	return res;
 }
 
-void testes(int *arr, int len)
+int * CriaVet(int *arr, int n)
 {
-	Data res;
-	clock_t begin, end;
+	int *vet = malloc(n * sizeof(int));
+	for (int i=0; i<n; i++) vet[i] = arr[i];
+	return vet;
+}
 
-	begin = time(NULL);
-	res = bubbleSort(arr, len);
-	end = time(NULL);
-	printf("bubble: %.2lfs res0: %d, res1: %d\n", (double)(end - begin), res.comps, res.swaps);
-	begin = time(NULL);
-	res = selectionSort(arr, len);
-	end = time(NULL);
-	printf("selection: %.2lfs\n", (double)(end - begin));
-	begin = time(NULL);
-	res = insertionSort(arr, len);
-	end = time(NULL);
-	printf("insertion: %.2lfs\n", (double)(end - begin));
+void testes(int *arr, int n)
+{
+	time_t start, end;
+	Data res1, res2, res3;
+	Data *res[3] = {&res1, &res2, &res3};
+	initData(res);
+	int *vet1, *vet2, *vet3;
+	vet1 = CriaVet(arr, n);
+	vet2 = CriaVet(arr, n);
+	vet3 = CriaVet(arr, n);
+	start = clock();
+	res1 = bubbleSort(vet1, n, res1);
+	end = clock();
+	printf("Bubble: %.3lfs Comparacoes: %ld, Trocas: %ld\n", (double)(end - start) / CLOCKS_PER_SEC, res1.comps, res1.swaps);
+	start = clock();
+	res2 = selectionSort(vet2, n, res2);
+	end = clock();
+	printf("Selection: %.3lfs Comparacoes: %ld, Trocas: %ld\n", (double)(end - start) / CLOCKS_PER_SEC, res2.comps, res2.swaps);
+	start = clock();
+	res3 = insertionSort(vet3, n, res3);
+	end = clock();
+	printf("Insertion: %.3lfs Comparacoes: %ld, Trocas: %ld\n", (double)(end - start) / CLOCKS_PER_SEC, res3.comps, res3.swaps);
+	free(vet1);
+	free(vet2);
+	free(vet3);
+	return;
+}
+
+void testarSort()
+{
+	int *vet = malloc(100 * sizeof(int));
+	Data res;
+	srand(time(NULL));
+	printf("============== Nao ordenados ==============\n");
+	for (int i=0; i<100; i++) {
+		vet[i] = (rand() % (99 - 1)) + 1;
+		printf("%d\n", vet[i]);
+	}
+	res = bubbleSort(vet, 100, res);
+	printf("============== Ordenados ==============\n");
+	for (int i=0; i<100; i++) printf("%d\n", vet[i]);
+	return;
 }
 
 int main()
@@ -113,14 +135,17 @@ int main()
 			arr1[j] = j;
 			arr2[j] = k;
 			k--;
-			arr3[j] = (rand() % (99 - 1)) + 1;
+			arr3[j] = (rand() % 99) + 1;
 		}
 		printf("====== Crescente [%dk] ======\n", len[i] / 1000);
 		testes(arr1, len[i]);
+		printf("\n");
 		printf("====== Decrescente [%dk] ======\n", len[i] / 1000);
 		testes(arr2, len[i]);
+		printf("\n");
 		printf("====== Aleatorio [%dk] ======\n", len[i] / 1000);
 		testes(arr3, len[i]);
+		printf("\n");
 	}
 	return 0;
 }
